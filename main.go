0803cd9992241f1c342a174e699b137b592b0318
bzhn <bzhn@duck.com>
2022-09-05 20:57:01 +0300

    Add new user. Add, remove reps

--- main.go
+++ main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
+	"strconv"
 
 	db "github.com/bzhn/dayrepsbot/pkg/db"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
 )
 
-func init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Panic(err)
-	}
-
-}
+var IKBresetConfirm = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Reset all today's progress", "reset_today"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("No, don't remove my progress", "reset_cancel"),
+	),
+)
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	bot, err := tgbotapi.NewBotAPI(db.TGToken)
 	if err != nil {
-		log.Panic(err)
+		panic(err)
 	}
 
 	bot.Debug = true
@@ -33,6 +34,32 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
+		if update.CallbackQuery != nil {
+			// msg := tgbotapi.NewMessage(update.CallbackQuery.From.ID, "")
+
+			switch update.CallbackQuery.Data {
+			case "reset_today":
+				err := db.ClearTodayProgress(update.CallbackQuery.From.ID)
+				if err != nil {
+					panic(err)
+				}
+				msg := tgbotapi.NewEditMessageText(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, "Successfully removed your reps")
+				bot.Send(msg)
+
+			case "reset_cancel":
+				err := db.ClearTodayProgress(update.CallbackQuery.From.ID)
+
+				if err != nil {
+					panic(err)
+				}
+				msg := tgbotapi.NewDeleteMessage(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID)
+				bot.Send(msg)
+
+			}
+
+			continue
+		}
+
 		if update.Message == nil || update.Message.Text == "" { // ignore any non-Message updates
 			continue
 		}
@@ -44,13 +71,24 @@ func main() {
 			// Extract the command from the Message.
 			switch update.Message.Command() {
 			case "start":
-				db.AddUser(update.Message.From.ID)
+				fmt.Println("Got start command")
+				err := db.AddUser(update.Message.From.ID)
+
+				if err != nil {
+					panic(err)
+				}
+
+				msg.Text = "Successfully added you to database."
+
 			case "help":
 				msg.Text = "I understand /sayhi and /status."
 			case "sayhi":
 				msg.Text = "Hi :)"
 			case "status":
 				msg.Text = "I'm ok."
+			case "reset":
+				msg.Text = "Are you sure you want to remove your progress?"
+				msg.ReplyMarkup = IKBresetConfirm
 			default:
 				msg.Text = "I don't know that command"
 			}
@@ -58,7 +96,33 @@ func main() {
 			if _, err := bot.Send(msg); err != nil {
 				log.Panic(err)
 			}
+			continue
 
+		}
+
+		if n, err := strconv.Atoi(update.Message.Text); err == nil {
+			if n > 2147483647 {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, amount of reps is too big!")
+				if _, err := bot.Send(msg); err != nil {
+					panic(err)
+				}
+			}
+
+			curReps, err := db.Increment(update.Message.From.ID, n)
+			if err != nil {
+				panic(err)
+			}
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("You have done %d reps today!", curReps))
+
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
+		} else {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, I don't understand")
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
 		}
 
 	}
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -7,12 +7,21 @@ import (
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var Mysql_connection string
 var DB *sql.DB
+var err error
+var TGToken string
 
 func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	TGToken = os.Getenv("TELEGRAM_API_TOKEN")
 
 	Mysql_connection = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
 		os.Getenv("MYSQL_USER"),
@@ -21,18 +30,74 @@ func init() {
 		os.Getenv("MYSQL_PORT"),
 		os.Getenv("MYSQL_DB_NAME"))
 
-	DB, err := sql.Open("mysql", Mysql_connection)
+	DB, err = sql.Open("mysql", Mysql_connection)
 	if err != nil {
 		panic(err.Error())
 	}
-	defer DB.Close()
 }
 
-func userExists(userID uint32) bool {
-
+func userExists(userID int64) bool {
+	return false
 }
 
-func Increment(userID uint32, reps int) (int, error) {
+func Increment(userID int64, reps int) (int, error) {
 
-	return 0, nil
+	stmt, err := DB.Prepare(`INSERT INTO reps (telegram_id, reps_amount, reps_date) VALUES 
+	(?, ?, date(NOW()))
+	ON DUPLICATE KEY UPDATE reps_amount = reps_amount + ?;`)
+	if err != nil {
+		return 0, err
+	}
+
+	res, err := stmt.Exec(userID, reps, reps)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println(res)
+
+	stmt, err = DB.Prepare(`SELECT (r.reps_amount) FROM reps r
+	WHERE r.telegram_id = ? AND r.reps_date = date(NOW());
+	`)
+	if err != nil {
+		return 0, err
+	}
+
+	var curReps int
+	stmt.QueryRow(userID).Scan(&curReps)
+
+	return curReps, nil
+}
+
+func AddUser(userID int64) error {
+	stmt, err := DB.Prepare(`INSERT INTO person (telegram_id, state_id, first_interaction, last_interaction) VALUES
+	(?, 0, NOW(), NOW())
+	ON DUPLICATE KEY UPDATE telegram_id = telegram_id, last_interaction = NOW();`)
+	if err != nil {
+		return err
+	}
+
+	res, err := stmt.Exec(userID)
+	if err != nil {
+		return err
+	}
+	fmt.Println(res)
+
+	return nil
+}
+
+func ClearTodayProgress(userID int64) error {
+
+	stmt, err := DB.Prepare(`DELETE FROM reps
+	WHERE telegram_id = ? AND reps_date = date(NOW());`)
+	if err != nil {
+		return err
+	}
+
+	res, err := stmt.Exec(userID)
+	if err != nil {
+		return err
+	}
+	fmt.Println(res)
+
+	return nil
 }
